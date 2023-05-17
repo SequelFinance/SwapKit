40508ba1375e1e374d93b83014e9b1364ed8a2fd
Damian <damian@sequel.fi>
2023-05-17 12:11:26 +0400

    fixes bsc fee estimation

---
+++ .changeset/smooth-dots-provide.md
@@ -0,0 +1,5 @@
+---
+'@sequelfinance/toolbox-evm': patch
+---
+
+Fixes Fees for BSC chain
--- packages/toolbox-evm/src/toolbox/bsc.ts
+++ packages/toolbox-evm/src/toolbox/bsc.ts
@@ -1,7 +1,9 @@
 import { Provider } from '@ethersproject/abstract-provider';
 import { Signer } from '@ethersproject/abstract-signer';
-import { BigNumber } from '@ethersproject/bignumber';
+import { BigNumber, BigNumberish } from '@ethersproject/bignumber';
+import { hexlify } from '@ethersproject/bytes';
 import { Web3Provider } from '@ethersproject/providers';
+import { toUtf8Bytes } from '@ethersproject/strings';
 import { baseAmount } from '@sequelfinance/helpers';
 import { AssetEntity, getSignatureAssetFor } from '@sequelfinance/swapkit-entities';
 import {
@@ -9,11 +11,18 @@ import {
   BaseDecimal,
   Chain,
   ChainId,
+  ContractAddress,
+  EIP1559TxParams,
+  erc20ABI,
+  EVMChain,
+  FeeOption,
   RPCUrl,
   TxHistoryParams,
 } from '@sequelfinance/types';
 
-import { BaseEVMToolbox, CovalentApi } from '../index.js';
+import { ApproveParams, BaseEVMToolbox, CovalentApi, TransferParams } from '../index.js';
+
+import { getAssetEntity, getTokenAddress, isWeb3Provider, MAX_APPROVAL } from './BaseEVMToolbox.js';
 
 const BSC_CHAIN_ID = ChainId.BinanceSmartChain;
 
@@ -79,9 +88,192 @@ export const BSCToolbox = ({
 }) => {
   const api = new CovalentApi({ apiKey: covalentApiKey });
 
+  const baseToolbox = BaseEVMToolbox({ provider, signer });
+
+  const estimateGasPrices = async (provider: Provider) => {
+    try {
+      const gasPrice = await provider.getGasPrice();
+
+      if (!gasPrice) throw new Error('No fee data available');
+
+      return {
+        [FeeOption.Average]: {
+          gasPrice: baseAmount(gasPrice, BaseDecimal.ETH),
+        },
+        [FeeOption.Fast]: {
+          gasPrice: baseAmount(gasPrice.mul(3).div(2), BaseDecimal.ETH),
+        },
+        [FeeOption.Fastest]: {
+          gasPrice: baseAmount(gasPrice.mul(2), BaseDecimal.ETH),
+        },
+      };
+    } catch (error) {
+      throw new Error(
+        `Failed to estimate gas price: ${(error as any).msg ?? (error as any).toString()}`,
+      );
+    }
+  };
+
+  const approve = async (
+    provider: Provider,
+    {
+      assetAddress,
+      spenderAddress,
+      feeOptionKey = FeeOption.Fastest,
+      amount,
+      walletIndex = 0,
+      gasLimitFallback,
+      from,
+      nonce,
+    }: ApproveParams,
+    signer?: Signer,
+  ) => {
+    const { gasPrice } = (await estimateGasPrices(provider))[feeOptionKey];
+
+    const funcParams = [spenderAddress, amount?.amount() || MAX_APPROVAL, { from }];
+
+    const functionCallParams = {
+      walletIndex,
+      contractAddress: assetAddress,
+      abi: erc20ABI,
+      funcName: 'approve',
+      funcParams,
+      signer,
+    };
+
+    const gasLimit = await baseToolbox
+      .estimateCall(functionCallParams)
+      .catch(() => BigNumber.from(gasLimitFallback));
+
+    if (isWeb3Provider(provider)) {
+      return await baseToolbox.EIP1193SendTransaction(
+        await baseToolbox.createContractTxObject(functionCallParams),
+      );
+    }
+
+    return baseToolbox.call({
+      ...functionCallParams,
+      funcParams: [
+        ...funcParams.slice(0, funcParams.length - 1),
+        {
+          from,
+          gasPrice,
+          gasLimit,
+          nonce,
+        },
+      ],
+    });
+  };
+
+  const transfer = async (
+    provider: Provider | Web3Provider,
+    {
+      asset,
+      memo,
+      amount,
+      recipient,
+      feeOptionKey = FeeOption.Fast,
+      gasLimit,
+      gasPrice,
+      data,
+      from,
+      nonce,
+    }: TransferParams,
+    signer?: Signer,
+  ) => {
+    if (!signer) throw new Error('Signer is not defined');
+    // TODO: create a method that creates the base tx object! @towan
+    const txAmount = amount.amount();
+    const parsedAsset: AssetEntity = getAssetEntity(asset);
+    const chain = parsedAsset.L1Chain as EVMChain;
+    const chainId = (await provider.getNetwork()).chainId;
+    const assetAddress = getTokenAddress(parsedAsset, chain);
+    const isGasAddress = assetAddress === ContractAddress[chain];
+    const gasFees = (await estimateGasPrices(provider))[feeOptionKey];
+    const overrides = {
+      gasLimit:
+        gasLimit || (await baseToolbox.estimateGasLimit({ asset, recipient, amount, memo, from })),
+      gasPrice: gasPrice || gasFees.gasPrice,
+      nonce: nonce || (await provider.getTransactionCount(from)),
+      from,
+    };
+
+    if (assetAddress && !isGasAddress) {
+      // Transfer ERC20
+      return baseToolbox.call({
+        contractAddress: assetAddress,
+        abi: erc20ABI,
+        funcName: 'transfer',
+        funcParams: [recipient, txAmount, overrides],
+      });
+    }
+
+    // Transfer ETH
+    const txObject = {
+      to: recipient,
+      value: txAmount,
+      chainId,
+      data: data || (memo ? hexlify(toUtf8Bytes(memo)) : undefined),
+      ...overrides,
+    };
+
+    return baseToolbox.sendTransaction(txObject, feeOptionKey);
+  };
+
+  const sendTransaction = async (
+    provider: Provider,
+    tx: EIP1559TxParams & { gasPrice: BigNumberish },
+    feeOptionKey: FeeOption = FeeOption.Fast,
+    signer?: Signer,
+  ) => {
+    if (!signer) throw new Error('Signer not defined');
+    const address = tx.from || (await signer.getAddress());
+    const nonce = tx.nonce || (await provider.getTransactionCount(address));
+    const chainId = (await provider.getNetwork()).chainId;
+
+    const { gasPrice } = tx;
+    const feeData = (await estimateGasPrices(provider))[feeOptionKey];
+
+    let gasLimit: BigNumber;
+    try {
+      gasLimit = tx.gasLimit
+        ? BigNumber.from(tx.gasLimit)
+        : (await provider.estimateGas(tx)).mul(110).div(100);
+    } catch (error) {
+      throw new Error(`Error estimating gas limit: ${JSON.stringify(error)}`);
+    }
+    try {
+      const { value, ...transaction } = tx;
+      const txObject = {
+        ...transaction,
+        chainId,
+        gasPrice: BigNumber.from(gasPrice || feeData.gasPrice).toHexString(),
+        gasLimit: gasLimit.toHexString(),
+        nonce,
+        ...(value && !BigNumber.from(value).isZero() ? { value } : {}),
+      };
+
+      if (isWeb3Provider(provider)) {
+        return await baseToolbox.EIP1193SendTransaction(txObject);
+      }
+
+      const txHex = await signer.signTransaction(txObject);
+      const response = await provider.sendTransaction(txHex);
+
+      return typeof response?.hash === 'string' ? response.hash : response;
+    } catch (error) {
+      throw new Error(`Error sending transaction: ${JSON.stringify(error)}`);
+    }
+  };
+
   return {
-    ...BaseEVMToolbox({ provider, signer }),
-    getTransactionData: (txHash: string) => getTransactionData(api, txHash),
+    ...baseToolbox,
+    estimateGasPrices: () => estimateGasPrices(provider),
+    transfer: (params: TransferParams) => transfer(provider, params, signer),
+    approve: (params: ApproveParams) => approve(provider, params, signer),
+    sendTransaction: (tx: EIP1559TxParams & { gasPrice: BigNumberish }, feeOptionKey: FeeOption) =>
+      sendTransaction(provider, tx, feeOptionKey, signer),
+    getTransactionData: (txId: string) => getTransactionData(api, txId),
     getTransactions: (params?: TxHistoryParams) => getTransactions(api, params),
     getNetworkParams,
     getBalance: (address: string, assets?: AssetEntity[]) =>
