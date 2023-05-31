d782e69d924b33a4aea2d99976fc016246d90361
Damian <damian@sequel.fi>
2023-05-31 17:26:32 +0400

    chore: remove old structured packages

--- packages/toolbox-evm/src/provider.ts
+++
@@ -1,6 +0,0 @@
-import { JsonRpcProvider } from '@ethersproject/providers';
-import { EVMChain, RPCUrlForChain } from '@sequelfinance/types';
-
-export const getProvider = (chain: EVMChain) => {
-  return new JsonRpcProvider(RPCUrlForChain[chain]);
-};
--- packages/toolbox-evm/src/types/Api.ts
+++
@@ -1,7 +0,0 @@
-import { Balance, Tx, TxsPage } from '@sequelfinance/types';
-
-export interface EvmApi<P> {
-  getTxInfo(params: P): Promise<Tx>;
-  getTransactionsForAddress(params: P): Promise<TxsPage>;
-  getBalance(params: P): Promise<Balance[]>;
-}
